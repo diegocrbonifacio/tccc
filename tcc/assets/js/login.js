1d0cc448587555347c9030c365d0a625a401ab4c
César <130309329+cesargehres@users.noreply.github.com>
2025-09-24 22:03:04 -0300

    feat: adicionados request de login, e salvar sessão.

--- tcc/assets/js/login.js
+++ tcc/assets/js/login.js
@@ -3,9 +3,66 @@ document.addEventListener('DOMContentLoaded', () => {
     const abaCadastro = document.getElementById('aba-cadastro');
     const formLogin = document.getElementById('form-login');
     const formCadastro = document.getElementById('form-cadastro');
+    const msgErro = document.getElementById('msg-error');
+
+    function salvarSessao({ usuario, access_token, refresh_token }) {
+        if (usuario) localStorage.setItem("usuario", JSON.stringify(usuario));
+        if (access_token) localStorage.setItem("access_token", access_token);
+        if (refresh_token) localStorage.setItem("refresh_token", refresh_token);
+    }
+
+    function obterSessao() {
+        const usuario = JSON.parse(localStorage.getItem("usuario"));
+        const access_token = localStorage.getItem("access_token");
+        const refresh_token = localStorage.getItem("refresh_token");
+        return { usuario, access_token, refresh_token };
+    }
+
+    function limparSessao() {
+        localStorage.removeItem("usuario");
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+    }
+
+
+    if (formLogin) {
+        msgErro.textContent = null
+        formLogin.addEventListener('submit', async (e) => {
+            e.preventDefault(); // evita reload da página
+
+            const dados = Object.fromEntries(new FormData(formLogin).entries());
+
+            console.log(JSON.stringify(dados));
+
+            try {
+                const response = await fetch("http://127.0.0.1:8000/usuarios/login/", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(dados)
+                });
+
+                const data = await response.json();
+                console.log("Resposta da API:", data);
+
+                if (data.result && data.result.tokens) {
+                    salvarSessao({
+                        usuario: data.result.usuario,
+                        access_token: data.result.tokens.access,
+                        refresh_token: data.result.tokens.refresh
+                    });
+                } else {
+                    msgErro.textContent = data.error;
+                }
+            } catch (err) {
+                console.error(err);
+                msgErro.textContent = "Erro ao conectar com o servidor"
+            }
+        });
+    }
 
     if (abaLogin) {
         abaLogin.addEventListener('click', () => {
+            msgErro.textContent = null
             ativarAba(abaLogin, formLogin);
         });
     }
